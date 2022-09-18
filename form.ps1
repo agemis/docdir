b21141438cd5bd7ce57bb3e81f59c4e3b982dfdc
agemis <antoine.gemis@gmail.com>
2022-09-18 15:01:45 +0200

    added clearform and a function to reset the ID

--- form.ps1
+++ form.ps1
@@ -9,16 +9,21 @@ Add-Type -AssemblyName System.Drawing
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 
+# Je me suis inspiré de :
 # https://www.rlvision.com/blog/a-drag-and-drop-gui-made-with-powershell/
+# https://adminscache.wordpress.com/2014/08/03/powershell-winforms-menu/
 
-$TOP = 20
+$TOP = 40
 $LEFT = 20
 $DELTA_WIDTH = 100
 $DELTA_HEIGHT = 30
 
+
+
+
 # Form
 $form                    = New-Object system.Windows.Forms.Form
-$form.ClientSize         = '1080,400'
+$form.ClientSize         = '1050,480'
 $form.text               = "Documentation directory name"
 $form.BackColor          = "#ffffff"
 $form.TopMost            = $false
@@ -26,10 +31,12 @@ $form.StartPosition = "CenterScreen"
 $form.MaximizeBox = $False
 $form.Topmost = $True
 $form.AllowDrop = $true
+$form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedDialog
 
-# Controls
+
+#region controls
 $AuthorLabel = New-Object system.Windows.Forms.Label
-$AuthorLabel.text = "Author"
+$AuthorLabel.text = "Author (a)"
 $AuthorLabel.AutoSize = $true
 $AuthorLabel.width = 25
 $AuthorLabel.Font = 'Microsoft Sans Serif,10'
@@ -43,7 +50,7 @@ $AuthorName.location = New-Object System.Drawing.Point(($LEFT + $DELTA_WIDTH),$T
 $AuthorName.Visible = $true
 
 $ProdYearLabel = New-Object system.Windows.Forms.Label
-$ProdYearLabel.text = "ProdYear"
+$ProdYearLabel.text = "Prod Year (y)"
 $ProdYearLabel.AutoSize = $true
 $ProdYearLabel.width = 25
 $ProdYearLabel.Font = 'Microsoft Sans Serif,10'
@@ -57,7 +64,7 @@ $ProdYearName.location = New-Object System.Drawing.Point(($LEFT + $DELTA_WIDTH),
 $ProdYearName.Visible = $true
 
 $PathLabel = New-Object system.Windows.Forms.Label
-$PathLabel.text = "Path"
+$PathLabel.text = "Path (p)"
 $PathLabel.AutoSize = $true
 $PathLabel.width = 25
 $PathLabel.Font = 'Microsoft Sans Serif,10'
@@ -71,7 +78,7 @@ $PathName.location = New-Object System.Drawing.Point(($LEFT + $DELTA_WIDTH),($TO
 $PathName.Visible = $true
 
 $CategoryLabel = New-Object system.Windows.Forms.Label
-$CategoryLabel.text = "Category"
+$CategoryLabel.text = "Category (c)"
 $CategoryLabel.AutoSize = $true
 $CategoryLabel.width = 25
 $CategoryLabel.Font = 'Microsoft Sans Serif,10'
@@ -85,7 +92,7 @@ $CategoryName.location = New-Object System.Drawing.Point(($LEFT + $DELTA_WIDTH),
 $CategoryName.Visible = $true
 
 $TitleLabel = New-Object system.Windows.Forms.Label
-$TitleLabel.text = "Title"
+$TitleLabel.text = "Title (t)"
 $TitleLabel.AutoSize = $true
 $TitleLabel.width = 25
 $TitleLabel.Font = 'Microsoft Sans Serif,10'
@@ -99,7 +106,7 @@ $TitleName.location = New-Object System.Drawing.Point(($LEFT + $DELTA_WIDTH),($T
 $TitleName.Visible = $true
 
 $RatingLabel = New-Object system.Windows.Forms.Label
-$RatingLabel.text = "Rating"
+$RatingLabel.text = "Rating (n)"
 $RatingLabel.AutoSize = $true
 $RatingLabel.width = 25
 $RatingLabel.Font = 'Microsoft Sans Serif,10'
@@ -113,7 +120,7 @@ $RatingName.location = New-Object System.Drawing.Point(($LEFT + $DELTA_WIDTH),($
 $RatingName.Visible = $true
 
 $TagLabel = New-Object system.Windows.Forms.Label
-$TagLabel.text = "Tag"
+$TagLabel.text = "Tag (g)"
 $TagLabel.AutoSize = $true
 $TagLabel.width = 25
 $TagLabel.Font = 'Microsoft Sans Serif,10'
@@ -127,7 +134,7 @@ $TagName.location = New-Object System.Drawing.Point(($LEFT + $DELTA_WIDTH),($TOP
 $TagName.Visible = $true
 
 $MetaTagLabel = New-Object system.Windows.Forms.Label
-$MetaTagLabel.text = "MetaTag"
+$MetaTagLabel.text = "MetaTag (m)"
 $MetaTagLabel.AutoSize = $true
 $MetaTagLabel.width = 25
 $MetaTagLabel.Font = 'Microsoft Sans Serif,10'
@@ -141,7 +148,7 @@ $MetaTagName.location = New-Object System.Drawing.Point(($LEFT + $DELTA_WIDTH),(
 $MetaTagName.Visible = $true
 
 $StoreYearLabel = New-Object system.Windows.Forms.Label
-$StoreYearLabel.text = "StoreYear"
+$StoreYearLabel.text = "Store Year (s)"
 $StoreYearLabel.AutoSize = $true
 $StoreYearLabel.width = 25
 $StoreYearLabel.Font = 'Microsoft Sans Serif,10'
@@ -156,7 +163,7 @@ $StoreYearName.Visible = $true
 
 
 $IDLabel = New-Object system.Windows.Forms.Label
-$IDLabel.text = "ID"
+$IDLabel.text = "ID (i)"
 $IDLabel.AutoSize = $true
 $IDLabel.width = 25
 $IDLabel.Font = 'Microsoft Sans Serif,10'
@@ -173,13 +180,27 @@ $IDName.Enabled = $false
 $DirectoryNameName = New-Object system.Windows.Forms.TextBox
 $DirectoryNameName.multiline = $false
 $DirectoryNameName.width = 1000
-$DirectoryNameName.text = "N/A"
+$DirectoryNameName.text = ""
 $DirectoryNameName.Font = 'Microsoft Sans Serif,10'
 $DirectoryNameName.location = New-Object System.Drawing.Point($LEFT, ($TOP + 10 * $DELTA_HEIGHT))
 $DirectoryNameName.Visible = $true
 $DirectoryNameName.ReadOnly = $true
 
+# -------------
 
+$Parts = New-Object system.Windows.Forms.ListBox
+$Parts.Height = 290
+$Parts.Width = 450
+$Parts.Font = 'Microsoft Sans Serif,10'
+$Parts.location  = New-Object System.Drawing.Point(($LEFT + 550),$TOP)
+#$Parts.Anchor = ([System.Windows.Forms.AnchorStyles]::Bottom -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right -bor [System.Windows.Forms.AnchorStyles]::Top)
+$Parts.IntegralHeight = $False
+$Parts.AllowDrop = $True
+ 
+
+
+
+# --------------
 
 
 $OKBtn                   = New-Object system.Windows.Forms.Button
@@ -187,7 +208,7 @@ $OKBtn.BackColor         = "#ff7b00"
 $OKBtn.text              = "OK"
 $OKBtn.width             = 90
 $OKBtn.height            = 30
-$OKBtn.location = New-Object System.Drawing.Point(($LEFT + 200),($TOP + 11 * $DELTA_HEIGHT))
+$OKBtn.location = New-Object System.Drawing.Point(($LEFT + 300),($TOP + 12 * $DELTA_HEIGHT))
 $OKBtn.Font              = 'Microsoft Sans Serif,10'
 $OKBtn.ForeColor         = "#ffffff"
 $OKBtn.Visible           = $true
@@ -197,7 +218,7 @@ $cancelBtn.BackColor             = "#ffffff"
 $cancelBtn.text                  = "Cancel"
 $cancelBtn.width                 = 90
 $cancelBtn.height                = 30
-$cancelBtn.location = New-Object System.Drawing.Point(($LEFT + 200 + $DELTA_WIDTH),($TOP + 11 * $DELTA_HEIGHT))
+$cancelBtn.location = New-Object System.Drawing.Point(($LEFT + 300 + $DELTA_WIDTH),($TOP + 12 * $DELTA_HEIGHT))
 $cancelBtn.Font                  = 'Microsoft Sans Serif,10'
 $cancelBtn.ForeColor             = "#000"
 $cancelBtn.DialogResult          = [System.Windows.Forms.DialogResult]::Cancel
@@ -207,12 +228,101 @@ $ComputeBtn.BackColor             = "#ffffff"
 $ComputeBtn.text              = "Compute"
 $ComputeBtn.width             = 90
 $ComputeBtn.height            = 30
-$ComputeBtn.location = New-Object System.Drawing.Point(($LEFT + 200 + 2 * $DELTA_WIDTH),($TOP + 11 * $DELTA_HEIGHT))
+$ComputeBtn.location = New-Object System.Drawing.Point(($LEFT + 300 + 2 * $DELTA_WIDTH),($TOP + 12 * $DELTA_HEIGHT))
 $ComputeBtn.Font              = 'Microsoft Sans Serif,10'
 $ComputeBtn.ForeColor             = "#000"
 $ComputeBtn.Visible           = $true
 
+#-----------
 
+
+$menuMain         = New-Object System.Windows.Forms.MenuStrip
+$menuFile         = New-Object System.Windows.Forms.ToolStripMenuItem
+$menuFile.Text = "File"
+
+$menuFileResetID    = New-Object System.Windows.Forms.ToolStripMenuItem
+$menuFileResetID.Text = "Reset ID (ctrl-alt-h)"
+$menuFileResetID.ShortcutKeys = "Control, Alt, H"
+$menuFileResetID.Add_Click({ResetID})
+[void]$menuFile.DropDownItems.Add($menuFileResetID)
+
+$menuParts         = New-Object System.Windows.Forms.ToolStripMenuItem
+$menuParts.Text = "Parts"
+$menuPartsSetAuthor    = New-Object System.Windows.Forms.ToolStripMenuItem
+$menuPartsSetAuthor.Text = "Set Author (alt-a)"
+$menuPartsSetAuthor.ShortcutKeys = "Alt, A"
+$menuPartsSetAuthor.Add_Click({PartsSetAuthor})
+[void]$menuParts.DropDownItems.Add($menuPartsSetAuthor)
+
+$menuPartsSetProdYear    = New-Object System.Windows.Forms.ToolStripMenuItem
+$menuPartsSetProdYear.Text = "Set Prod Year (alt-y)"
+$menuPartsSetProdYear.ShortcutKeys = "Alt, Y"
+$menuPartsSetProdYear.Add_Click({PartsSetprodYear})
+[void]$menuParts.DropDownItems.Add($menuPartsSetProdYear)
+
+$menuPartsSetPath    = New-Object System.Windows.Forms.ToolStripMenuItem
+$menuPartsSetPath.Text = "Set Path (alt-p)"
+$menuPartsSetPath.ShortcutKeys = "Alt, P"
+$menuPartsSetPath.Add_Click({PartsSetPath})
+[void]$menuParts.DropDownItems.Add($menuPartsSetPath)
+
+$menuPartsSetCategory    = New-Object System.Windows.Forms.ToolStripMenuItem
+$menuPartsSetCategory.Text = "Set Category (alt-c)"
+$menuPartsSetCategory.ShortcutKeys = "Alt, C"
+$menuPartsSetCategory.Add_Click({PartsSetCategory})
+[void]$menuParts.DropDownItems.Add($menuPartsSetCategory)
+
+$menuPartsSetTitle    = New-Object System.Windows.Forms.ToolStripMenuItem
+$menuPartsSetTitle.Text = "Set Title (alt-t)"
+$menuPartsSetTitle.ShortcutKeys = "Alt, T"
+$menuPartsSetTitle.Add_Click({PartsSetTitle})
+[void]$menuParts.DropDownItems.Add($menuPartsSetTitle)
+
+$menuPartsSetRating    = New-Object System.Windows.Forms.ToolStripMenuItem
+$menuPartsSetRating.Text = "Set Rating (alt-n)"
+$menuPartsSetRating.ShortcutKeys = "Alt, N"
+$menuPartsSetRating.Add_Click({PartsSetRating})
+[void]$menuParts.DropDownItems.Add($menuPartsSetRating)
+
+$menuPartsSetTag    = New-Object System.Windows.Forms.ToolStripMenuItem
+$menuPartsSetTag.Text = "Set Tag (alt-g)"
+$menuPartsSetTag.ShortcutKeys = "Alt, G"
+$menuPartsSetTag.Add_Click({PartsSetTag})
+[void]$menuParts.DropDownItems.Add($menuPartsSetTag)
+
+$menuPartsSetMetaTag    = New-Object System.Windows.Forms.ToolStripMenuItem
+$menuPartsSetMetaTag.Text = "Set Tag (alt-m)"
+$menuPartsSetMetaTag.ShortcutKeys = "Alt, M"
+$menuPartsSetMetaTag.Add_Click({PartsSetMetaTag})
+[void]$menuParts.DropDownItems.Add($menuPartsSetMetaTag)
+
+$menuPartsSetStoreYear    = New-Object System.Windows.Forms.ToolStripMenuItem
+$menuPartsSetStoreYear.Text = "Set Store Year (alt-s)"
+$menuPartsSetStoreYear.ShortcutKeys = "Alt, S"
+$menuPartsSetStoreYear.Add_Click({PartsSetStoreYear})
+[void]$menuParts.DropDownItems.Add($menuPartsSetStoreYear)
+
+$menuPartsSetID    = New-Object System.Windows.Forms.ToolStripMenuItem
+$menuPartsSetID.Text = "Set ID (alt-i)"
+$menuPartsSetID.ShortcutKeys = "Alt, I"
+$menuPartsSetID.Add_Click({PartsSetID})
+[void]$menuParts.DropDownItems.Add($menuPartsSetID)
+
+
+
+
+
+
+[void]$menuMain.Items.AddRange(@($menuFile, $menuParts))
+
+
+
+
+# TODO A continuer https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.toolstripstatuslabel?view=netframework-4.8 
+$statusStrip = New-Object System.Windows.Forms.StatusStrip
+$statusStrip.GripStyle = [System.Windows.Forms.ToolStripGripStyle]::Hidden  
+
+#endregion controls
 
 
 $form.CancelButton   = $cancelBtn
@@ -220,14 +330,16 @@ $form.Controls.Add($cancelBtn)
 
 # Add Controls to Form
 $form.SuspendLayout()
-$form.controls.AddRange(@($AuthorLabel,$AuthorName,$ProdYearLabel,$ProdYearName,$PathLabel,$PathName,$CategoryLabel,$CategoryName,$TitleLabel,$TitleName,$RatingLabel,$RatingName,$TagLabel,$TagName,$MetaTagLabel,$MetaTagName, `
-                                    $StoreYearLabel,$StoreYearName,$IDLabel,$IDName,$DirectoryNameName, `
+$form.controls.AddRange(@($menuMain,$AuthorLabel,$AuthorName,$ProdYearLabel,$ProdYearName,$PathLabel,$PathName,$CategoryLabel,$CategoryName,$TitleLabel,$TitleName,$RatingLabel,$RatingName,$TagLabel,$TagName,$MetaTagLabel,$MetaTagName, `
+                                    $StoreYearLabel,$StoreYearName,$IDLabel,$IDName,$DirectoryNameName, $Parts, $statusStrip, `
                                     $OKBtn,$cancelBtn,$ComputeBtn))
+
+                                    
+
 $form.ResumeLayout()
 
 
-# Event handlers
-
+#region eventhandlers
 $form_DragOver = [System.Windows.Forms.DragEventHandler]{
     if ($_.Data.GetDataPresent([Windows.Forms.DataFormats]::FileDrop)) # $_ = [System.Windows.Forms.DragEventArgs]
 	{
@@ -270,8 +382,10 @@ $form_DragDrop = [System.Windows.Forms.DragEventHandler]{
 $form_FormClosed = {
 	try
     {
-        #$listBox.remove_Click($button_Click)
-		$form.remove_DragOver($form_DragOver)
+        $Parts.remove_DragOver($Parts_DragOver)
+		$Parts.remove_DragDrop($Parts_DragDrop)
+		
+        $form.remove_DragOver($form_DragOver)
 		$form.remove_DragDrop($form_DragDrop)
 		$form.remove_FormClosed($Form_Cleanup_FormClosed)
 	}
@@ -282,12 +396,129 @@ $form_FormClosed = {
 
 
 
+$Parts_DragOver = [System.Windows.Forms.DragEventHandler]{
+	if ($_.Data.GetDataPresent([Windows.Forms.DataFormats]::FileDrop)) # $_ = [System.Windows.Forms.DragEventArgs]
+	{
+	    $_.Effect = 'Copy'
+	}
+	else
+	{
+	    $_.Effect = 'None'
+	}
+}
+	
+$Parts_DragDrop = [System.Windows.Forms.DragEventHandler]{
+    $a = $_.Data.GetData([Windows.Forms.DataFormats]::FileDrop) # $_ = [System.Windows.Forms.DragEventArgs]
+    if ($a.Count -eq 1)
+    {
+        if (Test-Path -Path $a[0] -PathType Container)
+        {
+            ClearForm
+            
+            $dir = Split-Path $a[0] -leaf
+            $dir.Split("_")
+            
+            
+            $Parts.Items.AddRange($dir.Split("_"))
+        }
+    }
+
+}
+
+
+function PartsSetAuthor {
+    if($Parts.SelectedItem)
+    {
+        $AuthorName.Text = $Parts.SelectedItem.ToString()
+    }
+}
+function PartsSetProdYear {
+    if($Parts.SelectedItem)
+    {
+        $ProdYearName.Text = $Parts.SelectedItem.ToString()
+    }
+}
+function PartsSetPath {
+    if($Parts.SelectedItem)
+    {
+        $PathName.Text = $Parts.SelectedItem.ToString()
+    }
+}
+function PartsSetCategory {
+    if($Parts.SelectedItem)
+    {
+        $CategoryName.Text = $Parts.SelectedItem.ToString()
+    }
+}
+function PartsSetTitle {
+    if($Parts.SelectedItem)
+    {
+        $TitleName.Text = $Parts.SelectedItem.ToString()
+    }
+}
+function PartsSetRating {
+    if($Parts.SelectedItem)
+    {
+        $RatingName.Text = $Parts.SelectedItem.ToString()
+    }
+}
+function PartsSetTag {
+    if($Parts.SelectedItem)
+    {
+        $TagName.Text = $Parts.SelectedItem.ToString()
+    }
+}
+function PartsSetMetaTag {
+    if($Parts.SelectedItem)
+    {
+        $MetaTagName.Text = $Parts.SelectedItem.ToString()
+    }
+}
+function PartsSetStoreYear {
+    if($Parts.SelectedItem)
+    {
+        $StoreYearName.Text = $Parts.SelectedItem.ToString()
+    }
+}
+function PartsSetID {
+    if($Parts.SelectedItem)
+    {
+        $IDName.Text = $Parts.SelectedItem.ToString()
+    }
+}
+
+
+function ResetID {
+    $IDName.Text = New-Guid
+}
+
+
+
+function ClearForm {
+    $Parts.Items.Clear()
+            
+    $AuthorName.Text = ""
+    $ProdYearName.Text = ""
+    $PathName.Text = ""
+    $CategoryName.Text = ""
+    $TitleName.Text = ""
+    $RatingName.Text = ""
+    $TagName.Text = ""
+    $MetaTagName.Text = ""
+    $StoreYearName.Text = ""
+    $IDName.Text = ""
+
+    $DirectoryNameName.Text = ""
+}
+
+
+
 function ComputeBtn_Clicked { 
     $doc = [DocDirectory]::new($AuthorName.Text, $ProdYearName.Text, $PathName.Text, $CategoryName.Text, $TitleName.Text, $RatingName.Text, $TagName.Text, $MetaTagName.Text, $StoreYearName.Text, $IDName.Text)
     Write-Host $doc
     $DirectoryNameName.text = $doc.DirectoryName()
 }
-
+#endregion eventhandlers
 
 
 # Wire events
@@ -295,6 +526,11 @@ function ComputeBtn_Clicked {
 $form.Add_DragOver($form_DragOver)
 $form.Add_DragDrop($form_DragDrop)
 $form.Add_FormClosed($form_FormClosed)
+
+
+$Parts.Add_DragOver($Parts_DragOver)
+$Parts.Add_DragDrop($Parts_DragDrop)
+
 
 
 $ComputeBtn.Add_Click({ ComputeBtn_Clicked })
@@ -312,8 +548,7 @@ $ComputeBtn.Add_Click({ ComputeBtn_Clicked })
 
 
 
-# classe a externaliser
-
+#region classes
 class DocDirectory
 {
     [string] $Author
@@ -376,3 +611,4 @@ class DocDirectory
 
 
 }
+#endregion classes
